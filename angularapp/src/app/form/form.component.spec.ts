09ea0f5a087bbd071b80bc36c5864b9dd5d84919
Iamneo <techadmin@iamneo.ai>
2023-08-03 16:44:16 +0000

    Backup Commit

--- angularapp/src/app/form/form.component.spec.ts
+++ angularapp/src/app/form/form.component.spec.ts
@@ -1,33 +1,23 @@
-import { Component, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-@Component({
-  selector: 'app-form',
-  templateUrl: './form.component.html',
-  styleUrls: ['./form.component.css']
-})
-export class FormComponent implements OnInit {
+import { FormComponent } from './form.component';
 
-  name : string = '';
-  calories : string = '';
-  image_url : string = '';
+describe('FormComponent', () => {
+  let component: FormComponent;
+  let fixture: ComponentFixture<FormComponent>;
 
-  list:any[] = [];
-  
-  @Output() public sendData = new EventEmitter<any>();
-  
-  constructor() { }
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ FormComponent ]
+    })
+    .compileComponents();
 
-  ngOnInit(): void {
-    this.name = 'apple';
-    this.calories='250';
-    this.image_url='https://png.pngtree.com/element_our/png/20181129/vector-illustration-of-fresh-red-apple-with-single-leaf-png_248312.jpg';
-  }
+    fixture = TestBed.createComponent(FormComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-  AddNewFood(){
-    console.log('AddNewFood Clicked')
-    this.list.push({id:this.list.length,name:this.name,calories:this.calories,url:this.image_url})
-    console.log(this.list)
-    this.sendData.emit(this.list);
-
-  }
-}
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
